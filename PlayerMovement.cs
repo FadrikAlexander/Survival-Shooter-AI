dbc7010cc623d9c2872444f765ff390a19f1f2fb
Fadrik <fadi.a.ashy@gmail.com>
2018-02-11 08:09:08 +0200

    Scripts Update
    Scripts Update on the current Asset Scripts

---
+++ PlayerMovement.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.AI;
+using UnitySampleAssets.CrossPlatformInput;
+
+namespace CompleteProject
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public float speed = 6f;            // The speed that the player will move at.
+
+        Vector3 movement;                   // The vector to store the direction of the player's movement.
+        Animator anim;                      // Reference to the animator component.
+        Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+        int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+        float camRayLength = 100f;          // The length of the ray from the camera into the scene.
+
+
+        //AI Stuff
+        NavMeshAgent nav;
+        float[,] PlacesCost;
+        Vector3[,] PosCost;
+
+       
+        void Awake ()
+        {
+            // Create a layer mask for the floor layer.
+            floorMask = LayerMask.GetMask ("Floor");
+            // Set up references.
+            anim = GetComponent <Animator> ();
+            playerRigidbody = GetComponent <Rigidbody> ();
+            nav = GetComponent<NavMeshAgent>();
+            nav.updateRotation = false;
+            PlacesCost = new float[5, 5];
+            PosCost = new Vector3[5, 5];
+            clearCosts();
+            StartCoroutine(startMovement());
+
+        }
+
+        void clearCosts()
+        {
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    PlacesCost[i, j] = 0 + Vector3.Distance(new Vector3(0, 0, 0), this.transform.position);
+                }
+            for (int ii = 0, i = -4;ii < 5 && i < 5; ii++, i +=2)
+                for (int jj = 0, j = -4; jj < 5 && j < 5; jj++, j +=2)
+                {
+                    PosCost[ii, jj] = new Vector3(this.transform.position.x + i, this.transform.position.y, this.transform.position.z + j);
+                }
+        }
+
+        void PrintCosts()
+        {
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    Debug.Log(PlacesCost[i, j]);
+                }
+        }
+
+        void SetCosts()
+        {
+            Collider[] Col = Physics.OverlapSphere(this.transform.position, 7.5f);
+            bool AbleMove = false ;
+
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    foreach (Collider c in Col)
+                    {
+                        switch (c.tag)
+                        {
+                            case "EnemyMove": AbleMove = true;
+                                PlacesCost[i, j] -= Vector3.Distance(PosCost[i, j], c.transform.position);
+                            break;
+                           /* case "Border": PlacesCost[i, j] += 2*Vector3.Distance(PosCost[i, j], c.transform.position);
+                            break;
+                            case "Cover": PlacesCost[i, j] += Vector3.Distance(PosCost[i, j], c.transform.position);
+                            break;*/
+                        }
+                    }
+                }
+            if (AbleMove)
+            {
+                float num = PlacesCost[0, 0];
+                Vector3 MovePos = Vector3.one;
+
+                for (int i = 0; i < 5; i++)
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (PlacesCost[i, j] < num)
+                        {
+                            num = PlacesCost[i, j];
+                            MovePos = PosCost[i, j];
+                        }
+                    }
+                Debug.Log(MovePos + " " + num);
+                nav.SetDestination(MovePos);
+            }
+            else
+                nav.SetDestination(new Vector3(0, 0, 0));
+        }
+
+        IEnumerator startMovement()
+        {
+            while (true)
+            {
+                clearCosts();
+                SetCosts();
+                yield return new WaitForSeconds(0.25f);
+            }
+        }
+
+
+        void FixedUpdate()
+        {
+            /*float h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+            float v = CrossPlatformInputManager.GetAxisRaw("Vertical");
+            Move (h, v);*/
+
+            Animating (nav.isStopped);
+        }
+        
+
+
+        void Move (float h, float v)
+        {
+            // Set the movement vector based on the axis input.
+            movement.Set (h, 0f, v);
+            
+            // Normalise the movement vector and make it proportional to the speed per second.
+            movement = movement.normalized * speed * Time.deltaTime;
+
+            // Move the player to it's current position plus the movement.
+            playerRigidbody.MovePosition (transform.position + movement);
+        }
+        void Animating (bool walking)
+        {
+            anim.SetBool ("IsWalking", walking);
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(this.transform.position, 7.5f);
+            float penaltyMin = float.MaxValue;
+            float penaltyMax = float.MinValue;
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    if (PlacesCost[i, j] > penaltyMax)
+                    {
+                        penaltyMax = PlacesCost[i, j];
+                    }
+                    if (PlacesCost[i, j] < penaltyMin)
+                    {
+                        penaltyMin = PlacesCost[i, j];
+                    }
+                }
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    Gizmos.color = Color.Lerp(Color.green, Color.red, Mathf.InverseLerp(penaltyMin, penaltyMax, PlacesCost[i, j]));
+                    Gizmos.DrawCube(PosCost[i, j], Vector3.one);
+                }
+        }
+    }
+}
---
+++ PlayerShooting.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnitySampleAssets.CrossPlatformInput;
+
+namespace CompleteProject
+{
+    public class PlayerShooting : MonoBehaviour
+    {
+        public int damagePerShot = 20;                  // The damage inflicted by each bullet.
+        public float timeBetweenBullets = 0.15f;        // The time between each shot.
+        public float range = 100f;                      // The distance the gun can fire.
+
+
+        float timer;                                    // A timer to determine when to fire.
+        Ray shootRay = new Ray();                       // A ray from the gun end forwards.
+        RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
+        int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
+        ParticleSystem gunParticles;                    // Reference to the particle system.
+        LineRenderer gunLine;                           // Reference to the line renderer.
+        AudioSource gunAudio;                           // Reference to the audio source.
+        Light gunLight;                                 // Reference to the light component.
+		public Light faceLight;								// Duh
+        float effectsDisplayTime = 0.2f;                // The proportion of the timeBetweenBullets that the effects will display for.
+
+        //AI Stuff
+        public GameObject Player;
+        GameObject mon;
+        Vector3 MonsterPos;
+        bool AbleShooting= false;
+        List<GameObject> Monsters = new List<GameObject>();
+
+        void Awake ()
+        {
+            shootableMask = LayerMask.GetMask ("Shootable");
+
+            gunParticles = GetComponent<ParticleSystem> ();
+            gunLine = GetComponent <LineRenderer> ();
+            gunAudio = GetComponent<AudioSource> ();
+            gunLight = GetComponent<Light> ();
+
+            StartCoroutine(StartShooting());
+        }
+
+        void Update ()
+        {
+            timer += Time.deltaTime;
+
+            if (AbleShooting && timer >= timeBetweenBullets && Time.timeScale != 0)
+            {
+                Player.transform.LookAt(MonsterPos);
+                Shoot ();
+            }
+       
+            if(timer >= timeBetweenBullets * effectsDisplayTime)
+            {
+                DisableEffects ();
+            }
+        }
+
+        IEnumerator StartShooting()
+        {
+            while (true)
+            {
+                if (Monsters.Count == 0)
+                {
+                    AbleShooting = false;
+                    yield return new WaitForSeconds(1f);
+                }
+                else
+                {
+                    float min = 100000;
+                    foreach (GameObject g in Monsters)
+                    {
+                        if(g!=null)
+                        {
+                            float minn = Vector3.Distance(this.transform.position, g.transform.position);
+                            if (minn < min)
+                            {
+                                min = minn;
+                                mon = g;
+                                MonsterPos = g.transform.position;
+                            }
+                        }
+                    }
+                    AbleShooting = true;
+                    yield return new WaitForSeconds(1f);
+                }
+
+            }
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Enemy")
+                Monsters.Add(other.gameObject);
+
+            StopAllCoroutines();
+            StartCoroutine(StartShooting());
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.tag == "Enemy")
+                Monsters.Remove(other.gameObject);
+
+            if (Monsters.Count == 0)
+            {
+                AbleShooting = false;
+                StopAllCoroutines();
+            }
+        }
+            
+
+
+        public void DisableEffects ()
+        {
+            gunLine.enabled = false;
+			faceLight.enabled = false;
+            gunLight.enabled = false;
+        }
+        void Shoot ()
+        {
+            // Reset the timer.
+            timer = 0f;
+
+            // Play the gun shot audioclip.
+            gunAudio.Play ();
+
+            // Enable the lights.
+            gunLight.enabled = true;
+			faceLight.enabled = true;
+
+            // Stop the particles from playing if they were, then start the particles.
+            gunParticles.Stop ();
+            gunParticles.Play ();
+
+            // Enable the line renderer and set it's first position to be the end of the gun.
+            gunLine.enabled = true;
+            gunLine.SetPosition (0, transform.position);
+
+            // Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
+            shootRay.origin = transform.position;
+            shootRay.direction = transform.forward;
+
+            // Perform the raycast against gameobjects on the shootable layer and if it hits something...
+            if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+            {
+                // Try and find an EnemyHealth script on the gameobject hit.
+                EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+
+                // If the EnemyHealth component exist...
+                if(enemyHealth != null)
+                {
+                    // ... the enemy should take damage.
+                    if (enemyHealth.TakeDamage(damagePerShot, shootHit.point))
+                    {
+                        AbleShooting = false;
+                        Monsters.Remove(mon.gameObject);
+                        MonsterPos.y = 0;
+                        StopAllCoroutines();
+                        StartCoroutine(StartShooting());
+                    }
+                }
+
+                // Set the second position of the line renderer to the point the raycast hit.
+                gunLine.SetPosition (1, shootHit.point);
+            }
+            // If the raycast didn't hit anything on the shootable layer...
+            else
+            {
+                // ... set the second position of the line renderer to the fullest extent of the gun's range.
+                gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+            }
+        }
+    }
+}
